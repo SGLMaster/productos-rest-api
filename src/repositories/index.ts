bba636f527f6cd811cf582cc6c808c630690152c
Willians Briceno <williansbriceno06@gmail.com>
2019-10-23 10:50:26 -0400

    Añadir repositorios de los usuarios y roles

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,1 +1,4 @@
 export * from './productos.repository';
+export * from './role.repository';
+export * from './user-role.repository';
+export * from './user.repository';
---
+++ src/repositories/role.repository.ts
@@ -0,0 +1,16 @@
+import {DefaultCrudRepository} from '@loopback/repository';
+import {Role, RoleRelations} from '../models';
+import {MemorydbDataSource} from '../datasources';
+import {inject} from '@loopback/core';
+
+export class RoleRepository extends DefaultCrudRepository<
+  Role,
+  typeof Role.prototype.id,
+  RoleRelations
+> {
+  constructor(
+    @inject('datasources.memorydb') dataSource: MemorydbDataSource,
+  ) {
+    super(Role, dataSource);
+  }
+}
